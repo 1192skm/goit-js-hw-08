ee13a8180a770bfb45469aa8c424893b0821070b
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-04-04 12:58:32 +0300

    Task2 Import vimeo & init player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,4 @@
+import Player from '@vimeo/player'; 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
-// import Player from '@vimeo/player';
-
-// const player = new Player('handstick', {
-//   id: 19231868,
-//   width: 640,
-// });
-
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
+const player = new Player(iframe);
+ 
