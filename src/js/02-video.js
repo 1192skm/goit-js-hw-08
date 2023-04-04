7ff84188a7dd2f44462e4767bd7beab73000e8ec
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-04-04 16:33:00 +0300

    Task 2 finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,14 @@
-import Player from '@vimeo/player'; 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'; 
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
  
-player.on('play', function () {
-  console.log('played the video!');
-});
+player.on('timeupdate', throttle(memoryPausedTime, 1000));
+
+function memoryPausedTime({seconds}) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
