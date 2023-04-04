fcf5c9a59e1e8f962cd93ee3aaa20b8c2a50603c
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-04-04 11:27:36 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
+
+const container = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+container.insertAdjacentHTML("beforeend", markup);
+
+new SimpleLightbox(".gallery a", {
+      captions: true,
+      captionsData: "alt",
+      captionDelay: 250,
+});
