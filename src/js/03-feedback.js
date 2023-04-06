2088799031c7be2ac40346de9cdf3204fe7e7615
Kostia Shaposhnyk <118116537+1192skm@users.noreply.github.com>
2023-04-06 11:29:56 +0300

    Task 3 correction

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,21 +2,19 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const KEY = 'feedback-form-state';
-// const email = document.querySelector('input');
-// const message = document.querySelector('textarea');
+
 form.addEventListener('submit', onFormSubmit);
 form.addEventListener('input', throttle(onFormInput, 400));
-// const data = {};
+
 let data = JSON.parse(localStorage.getItem(KEY)) || {};
 const { email, message } = form.elements;
 addTextArea();
 function onFormInput(evt) {
-  // data[evt.target.name] = evt.target.value;
   data = { email: email.value, message: message.value };
   localStorage.setItem(KEY, JSON.stringify(data));
 }
 function addTextArea() {
-  // const saveText = JSON.parse(localStorage.getItem('feedback-form-state'));
+
   if (data) {
     email.value = data.email || '';
     message.value = data.message || '';
@@ -24,12 +22,11 @@ function addTextArea() {
 }
 function onFormSubmit(evt) {
   evt.preventDefault();
-  // const { email, message } = form.elements;
+
   if (!email.value || !message.value) {
     return alert('Заповніть усі поля!');
-    //   return;
   }
-  // console.log(JSON.parse(localStorage.getItem("feedback-form-state")));
+  console.log(data);
   localStorage.removeItem(KEY);
   evt.currentTarget.reset();
   data = {};
@@ -37,40 +34,38 @@ function onFormSubmit(evt) {
 
 // ---------------------------------------------------
 // const form = document.querySelector(".feedback-form");
-// const email = document.querySelector('input');
-// const message = document.querySelector('textarea');
 
 // form.addEventListener('submit', onFormSubmit);
 // form.addEventListener('input', throttle(onFormInput, 400));
 
-// const data = { email: '', message: '' };
-// const saveText = JSON.parse(localStorage.getItem('feedback-form-state'));
+// let data = JSON.parse(localStorage.getItem('feedback-form-state'))|| {};
+// const { email, message } = form.elements;
 // addTextArea();
 
+
 // function onFormInput(evt) {
-//     data[evt.target.name] = evt.target.value;
-    
+//      data = { email: email.value, message: message.value };
 //     localStorage.setItem("feedback-form-state", JSON.stringify(data))
 // }
 
 // function onFormSubmit(evt) {
 //     evt.preventDefault();
-//     const { email, message } = form.elements;
+    
 //     if (!email.value || !message.value) {
-//       alert('Заповніть усі поля!');
-//       return;
+//      return alert('Заповніть усі поля!');
 //     }
 //     console.log(JSON.parse(localStorage.getItem("feedback-form-state"))|| {});
 //     localStorage.removeItem('feedback-form-state');
 //     evt.currentTarget.reset();
+//     data = {};
     
 // }
 
 // function addTextArea() {
     
-//     if (saveText) {
-//         email.value = saveText.email || '';
-//         message.value = saveText.message || '';
+//     if (data) {
+//         email.value = data.email || '';
+//         message.value = data.message || '';
 //     }
     
 // }
